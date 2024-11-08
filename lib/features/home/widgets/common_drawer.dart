f3b73fc1691c1ae8124f01683853d6fa74097fb7
Sahil Kudnaliya <sahilkundaliya.dev@gmail.com>
2024-11-08 17:55:04 +0530

    drawer added

--- lib/features/home/widgets/common_drawer.dart
+++ lib/features/home/widgets/common_drawer.dart
@@ -10,119 +10,149 @@ class CommonDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: AppColors.backgroundColors,
       child: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Stack(
           children: [
-            Container(
-              height: 200,
-              color: AppColors.customBlackColors,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 33,
-                          backgroundColor: Colors.grey,
-                          child: Icon(Icons.person,
-                              size: 55, color: AppColors.backgroundColors),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20),
-                          child: Text(
-                            'Dot Phasor',
-                            style: GoogleFonts.roboto(
-                              color: const Color(0xffEDF6FF),
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Divider(
-                      color: Colors.grey,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Container(
+                  height: 200,
+                  color: AppColors.customBlackColors,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              'Messages',
-                              style: GoogleFonts.roboto(
-                                color: const Color(0xffEDF6FF),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                              ),
+                            CircleAvatar(
+                              radius: 33,
+                              backgroundColor: Colors.grey,
+                              child: Icon(Icons.person,
+                                  size: 55, color: AppColors.backgroundColors),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(top: 4, left: 8),
-                              child: CircleAvatar(
-                                radius: 4,
-                                backgroundColor: AppColors.primary2Colors,
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Text(
+                                'Dot Phasor',
+                                style: GoogleFonts.roboto(
+                                  color: const Color(0xffEDF6FF),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            )
+                            ),
                           ],
                         ),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          color: const Color(0xffEDF6FF),
+                        const Divider(
+                          color: Colors.grey,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Messages',
+                                  style: GoogleFonts.roboto(
+                                    color: const Color(0xffEDF6FF),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 4, left: 8),
+                                  child: CircleAvatar(
+                                    radius: 4,
+                                    backgroundColor: AppColors.primary2Colors,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: const Color(0xffEDF6FF),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Your Trips',
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, color: const Color(0xffEDF6FF)),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Payment',
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, color: const Color(0xffEDF6FF)),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Uber Pass',
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, color: const Color(0xffEDF6FF)),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Settings',
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, color: const Color(0xffEDF6FF)),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Legal',
+                        style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: const Color(0xffEDF6FF)),
+                      ),
+                      Text(
+                        'v4.3712003',
+                        style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 14,
+                            color: const Color(0xffEDF6FF)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              title: Text(
-                'Your Trips',
-                style: GoogleFonts.roboto(
-                    fontSize: 20, color: const Color(0xffEDF6FF)),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Payment',
-                style: GoogleFonts.roboto(
-                    fontSize: 20, color: const Color(0xffEDF6FF)),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Uber Pass',
-                style: GoogleFonts.roboto(
-                    fontSize: 20, color: const Color(0xffEDF6FF)),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Settings',
-                style: GoogleFonts.roboto(
-                    fontSize: 20, color: const Color(0xffEDF6FF)),
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            Expanded(
-                child: Row(
-              children: [Text('data')],
-            ))
+            )
           ],
         ),
       ),
