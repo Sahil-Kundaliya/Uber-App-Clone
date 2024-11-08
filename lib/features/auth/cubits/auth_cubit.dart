35e168a972bc7716442da1e856c39459d6957c52
Sahil Kudnaliya <sahilkundaliya.dev@gmail.com>
2024-11-08 11:53:37 +0530

    openBottomSheet added
    openBottomSheet added

--- lib/features/auth/cubits/auth_cubit.dart
+++ lib/features/auth/cubits/auth_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uber_app_ui_challenge/constant/app_colors.dart';
 import 'package:uber_app_ui_challenge/features/auth/cubits/auth_state.dart';
 import 'package:uber_app_ui_challenge/features/auth/widgets/auth_code_widget.dart';
 import 'package:uber_app_ui_challenge/features/auth/widgets/auth_name_widget.dart';
@@ -46,6 +49,7 @@ class AuthCubit extends Cubit<AuthState> {
       case 4:
         currentAuthIndex = 3;
     }
+    emit(UpdateAuthState());
   }
 
   Widget getAuthWidget() {
@@ -67,4 +71,71 @@ class AuthCubit extends Cubit<AuthState> {
         return const SizedBox();
     }
   }
+
+  openBottomSheet({required BuildContext context}) {
+    return showModalBottomSheet(
+      context: context,
+      backgroundColor: Color(0xff1D1D1D),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.only()),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'You won’t be able to request a ride without adding a payment method',
+                style: GoogleFonts.roboto(
+                  fontSize: 24,
+                  color: AppColors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25, bottom: 20),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 58,
+                    width: double.infinity,
+                    decoration:
+                    const BoxDecoration(color: AppColors.customBlackColors),
+                    child: Center(
+                      child: Text(
+                        'Add Payment Method Now',
+                        style: GoogleFonts.roboto(
+                            color: const Color(0xffEDF6FF),
+                            fontSize: 21.6,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 58,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: AppColors.transparent,
+                      border: Border.all(color: Color(0xffEDF6FF))),
+                  child: Center(
+                    child: Text(
+                      'Do it Later',
+                      style: GoogleFonts.roboto(
+                          color: const Color(0xffEDF6FF),
+                          fontSize: 21.6,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
 }
--- lib/features/auth/pages/auth_page.dart
+++ lib/features/auth/pages/auth_page.dart
@@ -10,71 +10,6 @@ class AuthScreen extends StatelessWidget {
 
   static const String authScreen = '/AuthScreen';
 
-  openBottomSheet({required BuildContext context}) {
-    return showModalBottomSheet(
-      context: context,
-      backgroundColor: Color(0xff1D1D1D),
-      builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'You won’t be able to request a ride without adding a payment method',
-                style: GoogleFonts.roboto(
-                  fontSize: 24,
-                  color: AppColors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25, bottom: 20),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 58,
-                    width: double.infinity,
-                    decoration:
-                        const BoxDecoration(color: AppColors.customBlackColors),
-                    child: Center(
-                      child: Text(
-                        'Add Payment Method Now',
-                        style: GoogleFonts.roboto(
-                            color: const Color(0xffEDF6FF),
-                            fontSize: 21.6,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 58,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: AppColors.transparent,
-                      border: Border.all(color: Color(0xffEDF6FF))),
-                  child: Center(
-                    child: Text(
-                      'Do it Later',
-                      style: GoogleFonts.roboto(
-                          color: const Color(0xffEDF6FF),
-                          fontSize: 21.6,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -120,7 +55,7 @@ class AuthScreen extends StatelessWidget {
                       child: GestureDetector(
                         onTap: () {
                           if (authCubit.currentAuthIndex == 5) {
-                            openBottomSheet(context: context);
+                            authCubit.openBottomSheet(context: context);
                           } else {
                             authCubit.changeAuthIndex();
                           }
